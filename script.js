4820c5bbfa094c4b06b3f0bfb4080941dc44eaae
akshat-52 <52.akkey@gmail.com>
2023-06-04 23:51:55 +0530

    schema

--- script.js
+++ script.js
@@ -10,6 +10,12 @@
 //   })
 // }
 
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// const exampleEl = document.getElementById('tt1')
+// const tooltip = new bootstrap.Tooltip(exampleEl, options)
+
 document.getElementById("toastBtn").onclick = function() {
     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
     var toastList = toastElList.map(function(toastEl) {
